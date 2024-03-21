222f72da04538b679ae06859c8623a7dfbed889c
Man Njoro <elijohnmwoho@gmail.com>
2024-03-21 10:13:44 +0300

    "Update index.css with grid layout for cards and add card styling; update Projects.jsx to use card component with class"
    This commit updates the index.css file to use a grid layout for the cards with a minimum width of 300px and adds styling for the card component. The Projects.jsx file is updated to use the new card component with a class instead of inline styles. The media queries for small and medium screens have been removed as they are no longer necessary with the new grid layout.

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -2,20 +2,10 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export default function Projects(props) {
-  const styles = {
-    width: "20rem",
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "space-between",
-    border: "1px solid #ddd", // Faint border
-    borderRadius: "5px", // Rounded corners
-    boxShadow: "0 2px 4px rgba(0, 0, 0, 0.1)",
-    height: "350px"
-  };
   return (
     <>
       <Link to={props.url}>
-        <div className="card" style={styles}>
+        <div className="card">
           <img src={props.img} className="card-img-top" alt="project photo" />
           <div class="card-body">
             <h2 className="card-title text">{props.name}</h2>
